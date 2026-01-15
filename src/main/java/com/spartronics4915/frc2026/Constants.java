68767d2173abce0d6b4aaba9c41e89f45ce9fa99
Ehren von Ibsch <ehrenwolf42@icloud.com>
2026-01-15 00:12:31 -0800

    Add swerve to vision branch for pose estimation testing purposes

--- src/main/java/com/spartronics4915/frc2026/Constants.java
+++ src/main/java/com/spartronics4915/frc2026/Constants.java
@@ -4,6 +4,8 @@
 
 package com.spartronics4915.frc2026;
 
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,7 +13,11 @@ import org.photonvision.simulation.SimCameraProperties;
 
 import com.spartronics4915.frc2026.subsystems.vision.cameras.Camera;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.AngularVelocity;
 
 public final class Constants {
     public static class OperatorConstants {
@@ -19,12 +25,23 @@ public final class Constants {
         public static final int OPERATOR_CONTROLLER_PORT = 1;
     }
 
+    public static final class SwerveConstants {
+        public static final double TRACK_WIDTH = 22.475 / 12;
+        public static final double WHEEL_BASE = 22.475 / 12;
+        public static final double CHASSIS_RADIUS = Math.hypot(TRACK_WIDTH / 2, WHEEL_BASE / 2);
+
+        public static final double MAX_SPEED = Units.feetToMeters(24);
+        public static final AngularVelocity MAX_ANGULAR_SPEED = RadiansPerSecond.of(MAX_SPEED * Math.PI / CHASSIS_RADIUS);
+
+        public static Rotation2d TELEOP_HEADING_OFFSET = Rotation2d.fromDegrees(0.0);
+
+        public static boolean IS_FIELD_RELATIVE = false;
+
+        public static final double STICK_DEADBAND = 0.1;
+    }
+
     public static final class VisionConstants {
         public static final List<Camera> cameraList = new ArrayList<>();
-
-        public enum CameraType {
-            LUMA, LIMELIGHT
-        }
 
         public static final SimCameraProperties simCameraProperties = new SimCameraProperties();
             static {
@@ -34,6 +51,16 @@ public final class Constants {
                 simCameraProperties.setAvgLatencyMs(15);
                 simCameraProperties.setLatencyStdDevMs(5);
             }
+
+        public static final AprilTagFieldLayout rebuiltApriltagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+        
+        public enum CameraType {
+            LUMA, LIMELIGHT
+        }
+
+        public enum VisionState {
+            GLOBAL, LOCAL
+        }
     }
 
 }
