1692bacf9611260c41b9d2e1c3d6439dfc12082b
Gage <lockettgage14@gmail.com>
2026-01-31 14:14:59 -0800

    initialize Shooter subsystem in robot container to allow Networktables to work

--- src/main/java/com/spartronics4915/frc2026/RobotContainer.java
+++ src/main/java/com/spartronics4915/frc2026/RobotContainer.java
@@ -6,6 +6,7 @@ package com.spartronics4915.frc2026;
 
 import com.spartronics4915.frc2026.Constants.OperatorConstants;
 import com.spartronics4915.frc2026.commands.Autos;
+import com.spartronics4915.frc2026.subsystems.Shooter;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -23,6 +24,8 @@ public class RobotContainer {
 
     public RobotContainer() {
         configureBindings();
+
+        Shooter Shooter = new Shooter();
     }
 
     /**
