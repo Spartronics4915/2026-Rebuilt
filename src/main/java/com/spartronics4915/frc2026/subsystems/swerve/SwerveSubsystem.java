76c96a9bd03ac543d4f81fb135a22a3f13b40a2f
Goblin <elliotbrower@gmail.com>
2026-02-06 17:57:26 -0800

    fixed errors

--- src/main/java/com/spartronics4915/frc2026/subsystems/swerve/SwerveSubsystem.java
+++ src/main/java/com/spartronics4915/frc2026/subsystems/swerve/SwerveSubsystem.java
@@ -9,6 +9,7 @@ import com.spartronics4915.frc2026.util.ModeSwitchHandler.ModeSwitchInterface;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructPublisher;
@@ -32,10 +33,10 @@ public final class SwerveSubsystem extends SubsystemBase implements ModeSwitchIn
 
         try {
             swerveDrive = new SwerveParser(new File(Filesystem.getDeployDirectory(), swerveDir.directory)).createSwerveDrive(SwerveConstants.kMaxSpeed.in(SwerveConstants.MetersPerSecond),
-            // new Pose2d(new Translation2d(Meter.of(2),
-            //     Meter.of(5)),
-            //     Rotation2d.fromDegrees(180)
-            // )
+             //new Pose2d(new Translation2d(meter.of(2),
+             //    meter.of(5)),
+             //    Rotation2d.fromDegrees(180)
+             //),
                 guessStartingPosition()
             );
 
