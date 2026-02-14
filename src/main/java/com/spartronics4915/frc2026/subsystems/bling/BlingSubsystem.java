732dddc536fb5e28a0ed2140f9e93dc590b126db
Goblin <elliotbrower@gmail.com>
2026-02-14 14:43:50 -0800

    beginning initilizers of the bling subsystem

--- src/main/java/com/spartronics4915/frc2026/subsystems/bling/BlingSubsystem.java
+++ src/main/java/com/spartronics4915/frc2026/subsystems/bling/BlingSubsystem.java
@@ -14,4 +14,23 @@ import com.spartronics4915.frc2026.Constants;
 
 public class BlingSubsystem {
     private final CANdle candle = new CANdle(Constants.BlingConstants.CANdleID, "rio");
+    private final int LedCount = 300;
+    private XboxController joystick;
+
+    private Animation toAnimate = null;
+
+    public enum AnimationTypes {
+        ColorFlow,
+        Fire,
+        Larson,
+        Rainbow,
+        RgbFade,
+        SingleFade,
+        Strobe,
+        Twinkle,
+        TwinkleOff,
+        SetAll
+    }
+    
+    private AnimationTypes currentAnimation;
 }
