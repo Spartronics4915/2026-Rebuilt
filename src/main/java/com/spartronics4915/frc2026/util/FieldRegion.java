51658c875381cc5e01ce188e322e6f0a7fa91b67
Ehren von Ibsch <ehrenwolf42@icloud.com>
2026-02-26 00:21:19 -0800

    Add FieldRegion and FieldZoneMap utility classes, so we can use similar logic in the future

---
+++ src/main/java/com/spartronics4915/frc2026/util/FieldRegion.java
@@ -0,0 +1,23 @@
+package com.spartronics4915.frc2026.util;
+
+import edu.wpi.first.math.geometry.Translation2d;
+
+/**
+ * Represents a geometric region on a FRC field
+ */
+@FunctionalInterface
+public interface FieldRegion {
+    /** @return true if the given field-relative position is inside this region */
+    boolean contains(Translation2d position);
+
+    /** Creates a standard Axis-Aligned Bounding Box */
+    static FieldRegion rectangle(double minX, double maxX, double minY, double maxY) {
+        return pos -> pos.getX() >= minX && pos.getX() <= maxX 
+                   && pos.getY() >= minY && pos.getY() <= maxY;
+    }
+
+    /** Creates a circular boundary */
+    static FieldRegion circle(Translation2d center, double radiusMeters) {
+        return pos -> pos.getDistance(center) <= radiusMeters;
+    }
+}
