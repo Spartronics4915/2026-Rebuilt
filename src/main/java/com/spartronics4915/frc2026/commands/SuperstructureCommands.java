403ec8a5c1368da9c34a6e0c0428f0202bf79c05
DanTCA <daniil.here@gmail.com>
2026-03-01 16:37:02 -0800

    Fix pipeline not turning off

--- src/main/java/com/spartronics4915/frc2026/commands/SuperstructureCommands.java
+++ src/main/java/com/spartronics4915/frc2026/commands/SuperstructureCommands.java
@@ -78,6 +78,12 @@ public class SuperstructureCommands {
     //#region Controls
 
     public Command setPipelineState(PipelineState state) {
+        if (state == PipelineState.OFF) {
+            return Commands.parallel(
+                indexer.setStateCommand(state.indexerState),
+                feeder.setStateCommand(state.feederState)
+            );
+        }
         return Commands.sequence(
             Commands.waitUntil(() -> isShooterReady()),
             Commands.parallel(
