fc86629ebe4dbe1481c327814dbb0acd9d10270f
Ehren von Ibsch <ehrenwolf42@icloud.com>
2026-03-04 23:22:32 -0800

    Remove unused subsystems and utility classes for better vision sim testing

--- src/main/java/com/spartronics4915/frc2026/RobotContainer.java
+++ src/main/java/com/spartronics4915/frc2026/RobotContainer.java
@@ -6,44 +6,19 @@ package com.spartronics4915.frc2026;
 
 import com.spartronics4915.frc2026.Constants.OperatorConstants;
 import com.spartronics4915.frc2026.Constants.SwerveConstants.SwerveConfigurations;
-import com.spartronics4915.frc2026.autos.ComplexAutoChooser;
-import com.spartronics4915.frc2026.autos.DriveToPOI;
-import com.spartronics4915.frc2026.autos.NeutralZoneAutos;
-import com.spartronics4915.frc2026.autos.DriveToPOI.POI;
-import com.spartronics4915.frc2026.autos.ZoneTransition;
 import com.spartronics4915.frc2026.Constants.VisionConstants;
 
 import static com.spartronics4915.frc2026.Constants.SwerveConstants.AutoConstants.hubPose;
 import static com.spartronics4915.frc2026.Constants.SwerveConstants.AutoConstants.trenchTransform;
-import static com.spartronics4915.frc2026.Constants.*;
 import static com.spartronics4915.frc2026.subsystems.swerve.SwerveSubsystem.isFieldRelative;
 import static com.spartronics4915.frc2026.subsystems.swerve.SwerveSubsystem.teleopHeadingOffset;
 
-import java.util.Set;
-
 import com.spartronics4915.frc2026.commands.DriveCommand;
-import com.spartronics4915.frc2026.commands.SuperstructureCommands;
-import com.spartronics4915.frc2026.commands.SuperstructureCommands.PipelineState;
-import com.spartronics4915.frc2026.subsystems.control.AutoAimController;
-import com.spartronics4915.frc2026.subsystems.control.Superstructure;
-import com.spartronics4915.frc2026.subsystems.mechanisms.ClimberSubsystem;
-import com.spartronics4915.frc2026.subsystems.mechanisms.IntakeSubsystem;
-import com.spartronics4915.frc2026.subsystems.mechanisms.PivotSubsystem;
-import com.spartronics4915.frc2026.subsystems.mechanisms.IntakeSubsystem.IntakeState;
-import com.spartronics4915.frc2026.subsystems.mechanisms.head.HoodSubsystem;
-import com.spartronics4915.frc2026.subsystems.mechanisms.head.TurretSubsystem;
-import com.spartronics4915.frc2026.subsystems.mechanisms.pipeline.FeederSubsystem;
-import com.spartronics4915.frc2026.subsystems.mechanisms.pipeline.FeederSubsystem.FeederState;
-import com.spartronics4915.frc2026.subsystems.mechanisms.pipeline.IndexerSubsystem;
-import com.spartronics4915.frc2026.subsystems.mechanisms.pipeline.IndexerSubsystem.IndexerState;
-import com.spartronics4915.frc2026.subsystems.mechanisms.pipeline.ShooterSubsystem;
 import com.spartronics4915.frc2026.subsystems.swerve.SwerveSubsystem;
 import com.spartronics4915.frc2026.subsystems.vision.VisionConfiguration;
 import com.spartronics4915.frc2026.subsystems.vision.VisionSubsystem;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -56,17 +31,6 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-
-    public final HoodSubsystem hoodSubsystem = new HoodSubsystem();
-    public final TurretSubsystem turretSubsystem = new TurretSubsystem();
-
-    public final PivotSubsystem pivotSubsystem = new PivotSubsystem();
-    public final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
-    public final ClimberSubsystem climberSubsystem = new ClimberSubsystem();
-
-    public final IndexerSubsystem indexerSubsystem = new IndexerSubsystem();
-    public final FeederSubsystem feederSubsystem = new FeederSubsystem();
-    public final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
     
     public final SwerveSubsystem swerveSubsystem = new SwerveSubsystem(SwerveConfigurations.COMP_CHASSIS);
     public final VisionSubsystem visionSubsystem = new VisionSubsystem(
@@ -76,45 +40,14 @@ public class RobotContainer {
         swerveSubsystem::addVisionMeasurement, 
         swerveSubsystem
     );
-    
-    private final ZoneTransition transitionFactory = new ZoneTransition(swerveSubsystem, visionSubsystem);
-    private final DriveToPOI POIFactory = new DriveToPOI(swerveSubsystem, climberSubsystem);
-    private final NeutralZoneAutos neutralZoneFactory = new NeutralZoneAutos(swerveSubsystem);
-
-    private final ComplexAutoChooser autoChooser = new ComplexAutoChooser(transitionFactory, POIFactory, neutralZoneFactory, 10);
-    private final AutoAimController autoAimController = new AutoAimController(hoodSubsystem, turretSubsystem, swerveSubsystem, shooterSubsystem);
 
     private final CommandXboxController driverController = new CommandXboxController(OperatorConstants.DRIVER_CONTROLLER_PORT);
-    private final CommandXboxController operatorController = new CommandXboxController(OperatorConstants.OPERATOR_CONTROLLER_PORT);
     private final CommandXboxController debugController = new CommandXboxController(OperatorConstants.DEBUG_CONTROLLER_PORT);
 
     public DriveCommand driveCommand = new DriveCommand(driverController, debugController, swerveSubsystem);
-    public SuperstructureCommands superstructureCommands = new SuperstructureCommands(
-        hoodSubsystem, 
-        turretSubsystem, 
-        feederSubsystem, 
-        indexerSubsystem, 
-        shooterSubsystem, 
-        climberSubsystem, 
-        intakeSubsystem,
-        pivotSubsystem, 
-        autoAimController
-    );
-
-    public final Superstructure superstructure = new Superstructure(
-        swerveSubsystem, 
-        autoAimController, 
-        superstructureCommands
-    );
 
     public RobotContainer() {
         configureBindings();
-
-        SmartDashboard.putData("Auto-Aim Toggle", autoAimController.aimToggle());
-        SmartDashboard.putData("Auto-Shoot Toggle", autoAimController.shootingToggle());
-        SmartDashboard.putData("Reset Dynamics", superstructureCommands.resetDynamics());
-        SmartDashboard.putData("Pipeline On", superstructureCommands.setPipelineState(PipelineState.ON));
-        SmartDashboard.putData("Pipeline Off", superstructureCommands.setPipelineState(PipelineState.OFF));
     }
 
     /**
@@ -194,188 +127,10 @@ public class RobotContainer {
             })
         );
 
-        driverController.y().whileTrue(
-            POIFactory.generateCommand(POI.TOWER)
-        );
-
         driverController.leftTrigger().whileTrue(
             Commands.run(swerveSubsystem::lockModules, swerveSubsystem)
             .withName("X Brake Swerve")
         );
-
-        //#endregion
-        //#region Operator Controller
-
-        operatorController.povUp().whileTrue(
-            Commands.run(() -> {
-                hoodSubsystem.setSetpoint(hoodSubsystem.getPosition().plus(Rotation2d.fromDegrees(0.02)));
-            })
-        );
-
-        operatorController.povLeft().whileTrue(
-            Commands.run(() -> {
-                turretSubsystem.setSetpoint(turretSubsystem.getPosition().plus(Rotation2d.fromDegrees(0.5)));
-            })
-        );
-
-        operatorController.povRight().whileTrue(
-            Commands.run(() -> {
-                turretSubsystem.setSetpoint(turretSubsystem.getPosition().minus(Rotation2d.fromDegrees(0.5)));
-            })
-        );
-
-        operatorController.povDown().whileTrue(
-            Commands.run(() -> {
-                hoodSubsystem.setSetpoint(hoodSubsystem.getPosition().minus(Rotation2d.fromDegrees(0.02)));
-            })
-        );
-
-        operatorController.leftTrigger().onTrue(
-            superstructureCommands.intakeOn()
-        ).onFalse(
-            superstructureCommands.intakeOff()
-        );
-
-        operatorController.rightTrigger().whileTrue(
-            autoAimController.overrideShootCommand()
-        );
-
-        operatorController.leftBumper().whileTrue(
-            autoAimController.overrideTargetCommand(
-                AutoAimConstants.leftPassTarget
-            )
-        );
-
-        operatorController.rightBumper().whileTrue(
-            autoAimController.overrideTargetCommand(
-                AutoAimConstants.rightPassTarget
-            )
-        );
-
-        operatorController.a().onTrue(
-            intakeSubsystem.setStateCommand(IntakeState.OUTTAKE)
-        ).onFalse(
-            intakeSubsystem.setStateCommand(IntakeState.OFF)
-        );
-
-        operatorController.b().onTrue(
-            Commands.parallel(
-                feederSubsystem.setStateCommand(FeederState.REVERSE),
-                indexerSubsystem.setStateCommand(IndexerState.REVERSE)
-            )
-        ).onFalse(
-            Commands.parallel(
-                feederSubsystem.setStateCommand(FeederState.OFF),
-                indexerSubsystem.setStateCommand(IndexerState.OFF)
-            )
-        );
-
-        operatorController.x().onTrue(
-            autoAimController.shootingToggle()
-        );
-
-        operatorController.y().onTrue(
-            superstructureCommands.stowed()
-        );
-
-        operatorController.start().onTrue(
-            autoAimController.aimToggle()
-        );
-
-        //#endregion
-        //#region Debug Controller
-
-        // Driver nudge defs are in the driverController section
-
-        debugController.povUp().whileTrue(
-            Commands.run(() -> {
-                swerveSubsystem.drive(driverNudgeUp);
-            })
-        );
-
-        debugController.povLeft().whileTrue(
-            Commands.run(() -> {
-                swerveSubsystem.drive(driverNudgeLeft);
-            })
-        );
-
-        debugController.povRight().whileTrue(
-            Commands.run(() -> {
-                swerveSubsystem.drive(driverNudgeRight);
-            })
-        );
-
-        debugController.povDown().whileTrue(
-            Commands.run(() -> {
-                swerveSubsystem.drive(driverNudgeDown);
-            })
-        );
-
-        debugController.leftTrigger().onTrue(
-            superstructureCommands.intakeOn()
-        ).onFalse(
-            superstructureCommands.intakeOff()
-        );
-
-        debugController.rightTrigger().whileTrue(
-            autoAimController.overrideShootCommand()
-        );
-
-        debugController.leftBumper().onTrue(
-            Commands.runOnce(() -> {
-                swerveSubsystem.setMovementOverride(
-                    hubPose.minus(trenchTransform.times(swerveSubsystem.shouldFlip() ? -1 : 1)).getY()
-                );
-            })
-        ).onFalse(
-            Commands.runOnce(() -> {
-                swerveSubsystem.setMovementOverride(0.0);
-            })
-        );
-
-        debugController.rightBumper().onTrue(
-            Commands.runOnce(() -> {
-                swerveSubsystem.setMovementOverride(
-                    hubPose.plus(trenchTransform.times(swerveSubsystem.shouldFlip() ? -1 : 1)).getY()
-                );
-            })
-        ).onFalse(
-            Commands.runOnce(() -> {
-                swerveSubsystem.setMovementOverride(0.0);
-            })
-        );
-
-
-        debugController.a().onTrue(
-            Commands.runOnce(() -> {
-                teleopHeadingOffset = swerveSubsystem.getPose().getRotation();
-            })
-        );
-
-        debugController.b().onTrue(
-            Commands.runOnce(() -> {
-                isFieldRelative = !isFieldRelative;
-            })
-        );
-
-        debugController.x().onTrue(
-            autoAimController.shootingToggle()
-        );
-
-        debugController.y().whileTrue(
-            POIFactory.generateCommand(POI.TOWER)
-        );
-
-        debugController.back().onTrue(
-            superstructureCommands.resetDynamics()
-        );
-
-        debugController.start().onTrue(
-            autoAimController.aimToggle()
-        );
-
-        //#endregion
-
     }
 
     /**
@@ -384,6 +139,6 @@ public class RobotContainer {
      * @return the command to run in autonomous
      */
     public Command getAutonomousCommand() {
-        return Commands.defer(() -> autoChooser.getAuto(), Set.of(swerveSubsystem));
+        return Commands.none();
     }
 }
