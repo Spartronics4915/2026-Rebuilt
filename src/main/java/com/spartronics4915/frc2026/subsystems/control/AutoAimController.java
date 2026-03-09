faa49c47b1f8d207b95b71f524a80c3d1a37b0eb
Ehren von Ibsch <ehrenwolf42@icloud.com>
2026-03-08 23:36:24 -0700

    Add (not actual) vision compensation to auto-aim

--- src/main/java/com/spartronics4915/frc2026/subsystems/control/AutoAimController.java
+++ src/main/java/com/spartronics4915/frc2026/subsystems/control/AutoAimController.java
@@ -115,7 +115,7 @@ public class AutoAimController extends SubsystemBase {
         );
 
         autoAim.setCollisionMap(this::collidesWithHub);
-        autoAim.setProcessingCompensation(0.05);
+        autoAim.setProcessingCompensation(0.05 + 0.07);
     }
 
     @Override
