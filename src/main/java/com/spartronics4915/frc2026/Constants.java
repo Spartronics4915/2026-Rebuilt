27c56bff34bb796fb3ef4c535ae202783a1562ac
Ehren von Ibsch <ehrenwolf42@icloud.com>
2026-03-19 16:47:48 -0700

    Increase Pivot P a little bit

--- src/main/java/com/spartronics4915/frc2026/Constants.java
+++ src/main/java/com/spartronics4915/frc2026/Constants.java
@@ -515,7 +515,7 @@ public final class Constants {
         public static final int MOTOR_ID = 14;
         public static final int ENCODER_ID = 16;
 
-        public static final double P = 90;
+        public static final double P = 100;
         public static final double I = 0.0;
         public static final double D = 0.2;
 
