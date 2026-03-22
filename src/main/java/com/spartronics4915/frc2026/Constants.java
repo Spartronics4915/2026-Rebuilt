9addcf01c886bcbe5f63850ab9274425b7853e09
Ehren von Ibsch <ehrenwolf42@icloud.com>
2026-03-21 19:46:53 -0700

    Increase vision standard deviations

--- src/main/java/com/spartronics4915/frc2026/Constants.java
+++ src/main/java/com/spartronics4915/frc2026/Constants.java
@@ -371,8 +371,8 @@ public final class Constants {
             }
 
         public static final class StdDevConstants {
-            public static final double baseXYStdDev = 0.18;  // 0.5 — trust vision closer to odometry levels
-            public static final double baseThetaStdDev = 0.9;  // 0.5 — heading from vision is still less reliable
+            public static final double baseXYStdDev = 0.32;  // 0.18
+            public static final double baseThetaStdDev = 0.87;  // 0.5 — heading from vision is still less reliable
             public static final double ambiguityWeight = 1.0;  // 0.8
             public static final double areaWeight = 0.9;  // 0.6
             public static final double motionWeight = 0.3;  // 0.4
