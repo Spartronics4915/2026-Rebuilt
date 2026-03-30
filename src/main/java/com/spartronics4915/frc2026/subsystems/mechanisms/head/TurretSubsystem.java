72b99d528a2eb5d68fddd5213782a7914896f411
Ehren von Ibsch <ehrenwolf42@icloud.com>
2026-03-30 10:54:21 -0700

    Add SysId characterization to turret

--- src/main/java/com/spartronics4915/frc2026/subsystems/mechanisms/head/TurretSubsystem.java
+++ src/main/java/com/spartronics4915/frc2026/subsystems/mechanisms/head/TurretSubsystem.java
@@ -2,10 +2,12 @@ package com.spartronics4915.frc2026.subsystems.mechanisms.head;
 
 import static edu.wpi.first.units.Units.Rotations;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.Volts;
 
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.controls.PositionTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.TorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.CANcoder;
 
 import com.spartronics4915.frc2026.util.general.ModeSwitchHandler;
@@ -20,9 +22,12 @@ import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 import static com.spartronics4915.frc2026.Constants.TurretConstants.*;
 import static com.spartronics4915.frc2026.Constants.GeneralConstants.CAN_BUS;
@@ -37,6 +42,29 @@ public class TurretSubsystem extends SubsystemBase implements ModeSwitchInterfac
     private State targetState = new State();
 
     private final PositionTorqueCurrentFOC positionTorqueRequest = new PositionTorqueCurrentFOC(0.0);
+
+    private final TorqueCurrentFOC sysIdControl = new TorqueCurrentFOC(0.0);
+    private boolean isCharacterizing = false;
+
+    private final SysIdRoutine sysIdRoutine = new SysIdRoutine(
+        new SysIdRoutine.Config(
+            null,
+            Volts.of(4),
+            null, 
+            null
+        ),
+        new SysIdRoutine.Mechanism(
+            (Voltage volts) -> motor.setControl(sysIdControl.withOutput(volts.in(Volts))),
+            (log) -> {
+                log.motor("Turret")
+                    .voltage(Volts.of(motor.getTorqueCurrent().getValueAsDouble()))
+                    .angularPosition(motor.getPosition().getValue())
+                    .angularVelocity(motor.getVelocity().getValue())
+                    .angularAcceleration(motor.getAcceleration().getValue());
+            },
+            this
+        )
+    );
     
     private TurretClamp currentClamp;
     private Rotation2d minAngle;
@@ -69,6 +97,11 @@ public class TurretSubsystem extends SubsystemBase implements ModeSwitchInterfac
 
         motor.addSetpoint(() -> targetState.position, (setpoint) -> setSetpoint(Rotation2d.fromDegrees(setpoint)));
 
+        SmartDashboard.putData("Turret Quasistatic Forward", sysIdQuasistatic(Direction.kForward));
+        SmartDashboard.putData("Turret Quasistatic Reverse", sysIdQuasistatic(Direction.kReverse));
+        SmartDashboard.putData("Turret Dynamic Forward", sysIdDynamic(Direction.kForward));
+        SmartDashboard.putData("Turret Dynamic Reverse", sysIdDynamic(Direction.kReverse));
+
         SmartDashboard.putData("Turret 0", setSetpointCommand(Rotation2d.fromDegrees(0)));
         SmartDashboard.putData("Turret 180", setSetpointCommand(Rotation2d.fromDegrees(180)));
         SmartDashboard.putData("Turret Motor", motor);
@@ -92,7 +125,9 @@ public class TurretSubsystem extends SubsystemBase implements ModeSwitchInterfac
             .withPosition(targetState.position)
             .withVelocity(targetState.velocity);
             
-        motor.setControl(positionTorqueRequest);
+        if (!isCharacterizing) {
+            motor.setControl(positionTorqueRequest);
+        }
 
         appliedOutPublisher.accept(motor.getDutyCycle().getValueAsDouble());
         positionPublisher.accept(getPosition());
@@ -156,6 +191,18 @@ public class TurretSubsystem extends SubsystemBase implements ModeSwitchInterfac
 
     public Command setClampCommand(TurretClamp newClamp) {
         return this.runOnce(() -> setClamp(newClamp));
+    }
+
+    public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
+        return sysIdRoutine.quasistatic(direction)
+            .beforeStarting(() -> isCharacterizing = true)
+            .finallyDo(() -> isCharacterizing = false);
+    }
+
+    public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+        return sysIdRoutine.dynamic(direction)
+            .beforeStarting(() -> isCharacterizing = true)
+            .finallyDo(() -> isCharacterizing = false);
     }
 
     //#endregion
