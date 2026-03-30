25e5262553e79c98eea62e4db79e421f123feb3f
Ehren von Ibsch <ehrenwolf42@icloud.com>
2026-03-30 11:04:47 -0700

    Add SysId characterization to indexer

--- src/main/java/com/spartronics4915/frc2026/subsystems/mechanisms/pipeline/IndexerSubsystem.java
+++ src/main/java/com/spartronics4915/frc2026/subsystems/mechanisms/pipeline/IndexerSubsystem.java
@@ -1,17 +1,22 @@
 package com.spartronics4915.frc2026.subsystems.mechanisms.pipeline;
 
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.controls.TorqueCurrentFOC;
 import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
 import com.ctre.phoenix6.controls.VoltageOut;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 import static com.spartronics4915.frc2026.Constants.IndexerConstants.*;
+import static edu.wpi.first.units.Units.Volts;
 import static com.spartronics4915.frc2026.Constants.GeneralConstants.CAN_BUS;
 
 import com.spartronics4915.frc2026.util.general.ModeSwitchHandler;
@@ -25,6 +30,27 @@ public class IndexerSubsystem extends SubsystemBase implements ModeSwitchInterfa
     private double currentSetpoint;
 
     private final VelocityTorqueCurrentFOC velocityTorqueRequest = new VelocityTorqueCurrentFOC(0.0);
+
+    private final TorqueCurrentFOC sysIdControl = new TorqueCurrentFOC(0.0);
+    private boolean isCharacterizing = false;
+    private final SysIdRoutine sysIdRoutine = new SysIdRoutine(
+        new SysIdRoutine.Config(
+            null,
+            Volts.of(4),
+            null, 
+            null
+        ),
+        new SysIdRoutine.Mechanism(
+            (Voltage volts) -> motor.setControl(sysIdControl.withOutput(volts.in(Volts))),
+            (log) -> {
+                log.motor("Indexer")
+                    .voltage(Volts.of(motor.getTorqueCurrent().getValueAsDouble()))
+                    .angularVelocity(motor.getVelocity().getValue())
+                    .angularAcceleration(motor.getAcceleration().getValue());
+            },
+            this
+        )
+    );
 
     private final DoublePublisher appliedOutPublisher = NetworkTableInstance.getDefault().getTable("indexer").getDoubleTopic("applied out").publish();
     private final DoublePublisher rpsPublisher = NetworkTableInstance.getDefault().getTable("indexer").getDoubleTopic("rps").publish();
@@ -43,6 +69,11 @@ public class IndexerSubsystem extends SubsystemBase implements ModeSwitchInterfa
         ModeSwitchHandler.EnableModeSwitchHandler(this);
 
         motor.addSetpoint(() -> currentSetpoint, this::setSetpoint);
+
+        SmartDashboard.putData("Indexer Quasistatic Forward", sysIdQuasistatic(Direction.kForward));
+        SmartDashboard.putData("Indexer Quasistatic Reverse", sysIdQuasistatic(Direction.kReverse));
+        SmartDashboard.putData("Indexer Dynamic Forward", sysIdDynamic(Direction.kForward));
+        SmartDashboard.putData("Indexer Dynamic Reverse", sysIdDynamic(Direction.kReverse));
         
         SmartDashboard.putData("Indexer On", setStateCommand(IndexerState.FORWARD));
         SmartDashboard.putData("Indexer Off", setStateCommand(IndexerState.OFF));
@@ -57,11 +88,13 @@ public class IndexerSubsystem extends SubsystemBase implements ModeSwitchInterfa
             MAX_RPS
         );
 
-        if (currentSetpoint != 0) {
-            velocityTorqueRequest.Velocity = currentSetpoint;
-            motor.setControl(velocityTorqueRequest);
-        } else {
-            motor.setControl(new VoltageOut(0.0));
+        if (!isCharacterizing) {
+            if (currentSetpoint != 0) {
+                velocityTorqueRequest.Velocity = currentSetpoint;
+                motor.setControl(velocityTorqueRequest);
+            } else {
+                motor.setControl(new VoltageOut(0.0));
+            }
         }
 
         appliedOutPublisher.accept(motor.getDutyCycle().getValueAsDouble());
@@ -91,6 +124,18 @@ public class IndexerSubsystem extends SubsystemBase implements ModeSwitchInterfa
 
     public Command setStateCommand(IndexerState state){
         return setSetpointCommand(state.rps);
+    }
+
+    public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
+        return sysIdRoutine.quasistatic(direction)
+            .beforeStarting(() -> isCharacterizing = true)
+            .finallyDo(() -> isCharacterizing = false);
+    }
+
+    public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+        return sysIdRoutine.dynamic(direction)
+            .beforeStarting(() -> isCharacterizing = true)
+            .finallyDo(() -> isCharacterizing = false);
     }
 
     public enum IndexerState {
