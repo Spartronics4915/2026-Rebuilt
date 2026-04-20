9466984a84df2244d2db47f05254ac69f2fbfadc
DanTCA <daniil.here@gmail.com>
2026-04-20 09:54:19 -0700

    Re-zero intake pivot

--- src/main/java/com/spartronics4915/frc2026/Constants.java
+++ src/main/java/com/spartronics4915/frc2026/Constants.java
@@ -643,8 +643,8 @@ public final class Constants {
         public static final double LOWER_TIME = 1;
         public static final double MOTOR_MECHANISM_RATIO = 50.625;
 
-        public static final double MAGNET_OFFSET = 0.395264;
-        public static final SensorDirectionValue ENCODER_SENSOR_DIRECTION = SensorDirectionValue.Clockwise_Positive;
+        public static final double MAGNET_OFFSET = -0.400146;
+        public static final SensorDirectionValue ENCODER_SENSOR_DIRECTION = SensorDirectionValue.CounterClockwise_Positive;
 
         public static final Rotation2d MIN_ANGLE = Rotation2d.fromDegrees(-2);
         public static final Rotation2d MAX_ANGLE = Rotation2d.fromDegrees(132);
