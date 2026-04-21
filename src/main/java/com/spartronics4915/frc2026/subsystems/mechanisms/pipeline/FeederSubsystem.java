5568e4863b31f099e0dcdce9ddeeb69922dea184
Ehren von Ibsch <ehrenwolf42@icloud.com>
2026-04-20 22:32:07 -0700

    Return feeder speeds to old spindexer

--- src/main/java/com/spartronics4915/frc2026/subsystems/mechanisms/pipeline/FeederSubsystem.java
+++ src/main/java/com/spartronics4915/frc2026/subsystems/mechanisms/pipeline/FeederSubsystem.java
@@ -117,7 +117,7 @@ public class FeederSubsystem extends SubsystemBase implements ModeSwitchInterfac
             //);
             currentSetpoint = 22.01887 / (1 + Math.pow(
                 Math.E, 
-                -(((0.978997 * distanceToTargetSupplier.getAsDouble()) - 1.82251)))
+                -(((0.928997 * distanceToTargetSupplier.getAsDouble()) - 1.72251)))
             );
         }
 
