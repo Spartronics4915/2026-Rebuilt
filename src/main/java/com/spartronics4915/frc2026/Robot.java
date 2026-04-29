8a3d7a9032a50274eee624e91c9b16a5b94d6fe7
DanTCA <daniil.here@gmail.com>
2026-04-29 10:50:49 -0500

    Disable logging to file when in sim

--- src/main/java/com/spartronics4915/frc2026/Robot.java
+++ src/main/java/com/spartronics4915/frc2026/Robot.java
@@ -57,8 +57,10 @@ public class Robot extends TimedRobot {
     public void robotInit() {
         WebServer.start(5800, Filesystem.getDeployDirectory().getPath());
 
-        DataLogManager.start("/u/logs");
-        DriverStation.startDataLog(DataLogManager.getLog(), true);
+        if (Robot.isReal()) {
+            DataLogManager.start("/u/logs");
+            DriverStation.startDataLog(DataLogManager.getLog(), true);
+        }
 
         NetworkTable metaData = NetworkTableInstance.getDefault().getTable("Metadata");
         metaData.getStringTopic("Git: SHA").publish().accept(BuildConstants.GIT_SHA);
